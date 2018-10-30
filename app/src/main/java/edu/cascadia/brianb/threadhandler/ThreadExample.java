8d258f613e7a05e573018e595badb953780061d2
Brian Bansenauer <brianb@cascadia.edu>
2018-10-30 00:06:17 -0700

    updated TODOs

--- app/src/main/java/edu/cascadia/brianb/threadhandler/ThreadExample.java
+++ app/src/main/java/edu/cascadia/brianb/threadhandler/ThreadExample.java
@@ -13,14 +13,27 @@ import java.util.Locale;
 /* Code example based on http://www.techotopia.com/index.php/A_Basic_Overview_of_Android_Threads_and_Thread_handlers
  */
 public class ThreadExample extends Activity {
-
+    private static final int START_THREAD = 100;
+    private static final int END_THREAD = 200;
     int numThreads;
     TextView threadCounterView, myTextView;
-    //TODO define mHandler as an anonymous, custom inner class that extends Handler
-    Handler mHandler;
-        //TODO override handleMessage to update the UI
-        //TODO use the Message passed from the thread to update the UI
-        //TODO increment and decrement numThreads counter display
+
+    //Define mHandler as an anonymous, custom inner class that extends Handler
+    Handler mHandler = new Handler(){
+        //TODO @Override the handleMessage function to update the UI
+        {
+            //TODO test if message.what equals START_THREAD
+                // increment numThreads counter
+            //TODO else if the message.what equals END_THREAD,
+                //decrement numThreads counter
+                //use setText to update the myTextView to the string passed in the message
+            //TODO use setText to update the threadCounterView display
+        }
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +49,15 @@ public class ThreadExample extends Activity {
         Thread timeLapse = new Thread( new Runnable() {
             @Override
             public void run() {
-
-                //TODO: remove this as it breaks android's second rule for thread handling
+            //Note that this block is executing on a separate thread
+                //TODO: remove this setText method
+                //    it breaks the rule for thread handling, "Only update UIViews from UI thread"
                 myTextView.setText("Starting Thread");
 
                 //This is where the time goes while the thread is running
                 takeSomeTime(5);
 
-                //Send a message to the UI Thread through a Handler
+                //Sends a message with a Data Bundle to the UI Thread via Handler
                 if (mHandler != null) {
                     Message msg = mHandler.obtainMessage();
                     Bundle bundle = new Bundle();
@@ -52,19 +66,20 @@ public class ThreadExample extends Activity {
                     String dateString = dateformat.format(new Date());
                     bundle.putString("myKey", "It's now: " + dateString);
                     msg.setData(bundle);
+                    msg.what = END_THREAD;
                     mHandler.sendMessage(msg);
                 }
             }
         });
+        mHandler.sendEmptyMessage(START_THREAD);
         timeLapse.start();
 
+        //Notice that this is executed only on the UI Thread
         myTextView.setText("This might take a moment...");
         threadCounterView.setText("Thread Count: " + String.valueOf(numThreads));
-
-
     }
 
-    // Mimic time delay in a network activity
+    // Helper method to mimic a time delay such as network activity
     public void takeSomeTime(int seconds){
         long endTime = System.currentTimeMillis() + seconds*1000;
 
